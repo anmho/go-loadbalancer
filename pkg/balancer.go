6315bc43e96890febd95abaa22883dd0fe1ad907
Andrew Ho <andyminhtuanho@gmail.com>
2023-09-09 13:27:23 -0700

    Added basic reverse proxy functionality

--- pkg/balancer.go
+++ pkg/balancer.go
@@ -1,5 +1,7 @@
 package balancer
 
+import "fmt"
+
 type Rule struct {
 	ttlMS int
 	rate int
@@ -9,10 +11,16 @@ func NewRule(ttlMS int, rate int) Rule {
 	return Rule{ttlMS: ttlMS, rate: rate}
 }
 
+func (r*Rule) String() string {
+	return fmt.Sprintf("Rule: %d %d", r.ttlMS, r.rate)
+}
+
 type Balancer struct {
 	Rules map[string]string
 	Backends map[string]string
 }
+
+
 
 func (b *Balancer) AddRule (Rule) {
 
